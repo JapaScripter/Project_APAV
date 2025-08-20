4460698de65f514b15129a86b47d88db2947fcc4
JapaScripter <unilatem@gmail.com>
2025-08-19 23:58:26 -0300

    V.3.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,4 +77,39 @@ window.toggleMenu = function () {
 	if (menuIcon) menuIcon.src = isHidden ? '/assets/images/botoes/close.svg' : '/assets/images/botoes/menu.svg';
 };
 
+const cards = document.querySelectorAll(".card");
+const leftArrow = document.querySelector(".nav-arrow.left");
+const rightArrow = document.querySelector(".nav-arrow.right");
+let currentIndex = 0;
+let isAnimating = false;
 
+function updateCarousel(newIndex) {
+	if (isAnimating) return;
+	isAnimating = true;
+	currentIndex = (newIndex + cards.length) % cards.length;
+
+	cards.forEach((card, i) => {
+		const offset = (i - currentIndex + cards.length) % cards.length;
+		card.classList.remove("center", "left-1", "left-2", "right-1", "right-2", "hidden");
+		if (offset === 0) card.classList.add("center");
+		else if (offset === 1) card.classList.add("right-1");
+		else if (offset === 2) card.classList.add("right-2");
+		else if (offset === cards.length - 1) card.classList.add("left-1");
+		else if (offset === cards.length - 2) card.classList.add("left-2");
+		else card.classList.add("hidden");
+	});
+
+	setTimeout(() => isAnimating = false, 800);
+}
+
+leftArrow.addEventListener("click", () => updateCarousel(currentIndex - 1));
+rightArrow.addEventListener("click", () => updateCarousel(currentIndex + 1));
+cards.forEach((card, i) => card.addEventListener("click", () => updateCarousel(i)));
+
+let touchStartX = 0, touchEndX = 0;
+document.addEventListener("touchstart", e => touchStartX = e.changedTouches[0].screenX);
+document.addEventListener("touchend", e => { touchEndX = e.changedTouches[0].screenX; const diff = touchStartX - touchEndX; if (Math.abs(diff) > 50) diff > 0 ? updateCarousel(currentIndex + 1) : updateCarousel(currentIndex - 1); });
+
+document.addEventListener("keydown", (e) => { if (e.key === "ArrowLeft") updateCarousel(currentIndex - 1); else if (e.key === "ArrowRight") updateCarousel(currentIndex + 1); });
+
+updateCarousel(0);
